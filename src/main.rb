83aaa6a9f2f3bebe777861aa9fd62cb0421daac0
Christopher Bennett <cabnnt@live.unc.edu>
2018-02-18 19:16:12 -0500

    clarified prompt

--- src/main.rb
+++ src/main.rb
@@ -1,7 +1,7 @@
 require './spiral_memory.rb'
 
 puts('This program calculates the Manhattan distance to the first sum in \'spiral memory\''\
-     ' (defined here: https://gist.github.com/DavidIvy003/4ff8cd3eac07bcae2a2451dda28a0a54)'\
+     ' (defined here: https://en.wikipedia.org/wiki/Taxicab_geometry)'\
      ' larger than integer n (n >= 1).')
 
 puts('Input a value for n:')
